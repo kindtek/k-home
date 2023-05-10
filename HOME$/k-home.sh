801ec1ade2aad5e1b7bf92e714b5ae29583a92b4
kindtek@github.com <n8@kindtek.io>
2023-05-09 18:29:04 -0700

    add k-home update capability

---
+++ HOME$/k-home.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+timestamp=$(date -d "today" +"%Y%m%d%H%M%S")
+filename=k-home_$timestamp
+# log save location 
+mkdir -p logs
+tee "logs/$filename.sh" >/dev/null <<'TXT'
+#!/bin/bash
+set -x
+#               _________________________________________________                 #
+#                |||| |           Executing ...           | ||||                  #
+#              ---------------------------------------------------                #
+#
+                    docker buildx build ${build_cache} \
+                    --target dvlp_repo-k-home \
+                    --output type=local,dest=. \
+                    --build-arg LINUX=y \
+                    --progress=plain \
+                    . 2>&1 || exit<<'comment'
+comment
+# 
+#                -----------------------------------------------                   #
+#               |||||||||||||||||||||||||||||||||||||||||||||||||                  #
+#              ___________________________________________________                 #
+TXT
+
+# copy the command to the log first
+eval cat "logs/$filename.sh" 2>&1 | tee --append "logs/$filename.log" && \
+# execute .sh file && log all output
+bash "logs/${filename}.sh"  2>&1 | tee --append "logs/${filename}.log" 
