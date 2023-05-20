bb0dd4fe4e1021ba7c7e3e9e2701e5688d989ef2
kindtek@github.com <n8@kindtek.io>
2023-05-19 23:17:47 -0700

    set arg[1] to remove rule only

--- HOME_NIX/bridge-wsl2-net.ps1
+++ HOME_NIX/bridge-wsl2-net.ps1
@@ -1,21 +1,23 @@
 # source of the below self-elevating script: https://blog.expta.com/2017/03/how-to-self-elevate-powershell-script.html#:~:text=If%20User%20Account%20Control%20(UAC,select%20%22Run%20with%20PowerShell%22.
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -FilePath PowerShell.exe -Verb Runas -WindowStyle "Maximized" -ArgumentList $CommandLine
-        Exit
-    }
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath PowerShell.exe -Verb Runas -WindowStyle "Maximized" -ArgumentList $CommandLine
+    Exit
+  }
 }
 
+$remove_firewall_rule = $args[1]
 
 $remoteport = bash.exe -c "ifconfig eth0 | grep 'inet '"
 $found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
 
-if( $found ){
+if ( $found ) {
   $remoteport = $matches[0];
   Write-Output "IP found: $($matches[0])"
-} else{
+}
+else {
   Write-Output "The Script Exited, the ip address of WSL 2 cannot be found";
   exit;
 }
@@ -23,26 +25,28 @@ if( $found ){
 #[Ports]
 
 # All the ports you want to forward separated by comma
-$ports=@(80,443,10000,3000,5000);
+$ports = @(80, 443, 10000, 3000, 5000);
 
 
 #[Static ip]
 #You can change the addr to your ip config to listen to a specific address
-$addr='0.0.0.0';
+$addr = '0.0.0.0';
 $ports_a = $ports -join ",";
-
 
 #Remove Firewall Exception Rules
 Invoke-Expression "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock Outbound' ";
 Invoke-Expression "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock Inbound' ";
 
-#adding Exception Rules for inbound and outbound Rules
-Invoke-Expression "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock Outbound' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
-Invoke-Expression "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock Inbound' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+if ("$remove_firewall_rule" -eq "") {
+  #adding Exception Rules for inbound and outbound Rules
+  Invoke-Expression "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock Outbound' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+  Invoke-Expression "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock Inbound' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
 
-for( $i = 0; $i -lt $ports.length; $i++ ){
-  $port = $ports[$i];
-  Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
-  Invoke-Expression "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+  for ( $i = 0; $i -lt $ports.length; $i++ ) {
+    $port = $ports[$i];
+    Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
+    Invoke-Expression "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+  }
 }
+
 
