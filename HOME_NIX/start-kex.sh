e1b650041bf666977d7e7211a7c08b476261e0f9
kindtek <98300737+kindtek@users.noreply.github.com>
2023-06-03 02:21:10 -0700

    ubuntu -> kali

--- HOME_NIX/start-kex.sh
+++ HOME_NIX/start-kex.sh
@@ -9,10 +9,10 @@ while [ ! -d "/mnt/c/users/$win_user" ]; do
 
     save connection to which Windows home directory?
 
-        C:\\users\\__________\\Kubuntu-GUI.rdp 
+        C:\\users\\__________\\Kex-GUI.rdp 
 
         choose from:
-    " 
+    "
     ls -da /mnt/c/users/*/ | tail -n +4 | sed -r -e 's/^\/mnt\/c\/users\/([ A-Za-z0-9]*)*\/+$/\t\1/g'
     read -r -p "
 " win_user
@@ -21,7 +21,7 @@ sudo /etc/init.d/xrdp start
 if [ ! -f "/mnt/c/users/$win_user/KEX-GUI.rdp" ]; then
     sudo cp /mnt/data/HOME_WIN/KEX-GUI.rdp /mnt/c/users/$win_user/KEX-GUI.rdp
 fi
-pwsh -Command /mnt/c/Windows/system32/mstsc.exe /mnt/c/"$win_user"/Kubuntu-GUI.rdp /v:localhost:"$port_num" /admin /f /multimon || echo '
+pwsh -Command /mnt/c/Windows/system32/mstsc.exe /mnt/c/"$win_user"/Kex-GUI.rdp /v:localhost:"$port_num" /admin /f /multimon || echo '
 oops. no gui
 
  ¯\_(ツ)_/¯
